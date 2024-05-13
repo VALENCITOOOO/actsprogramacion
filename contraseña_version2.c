e68639e2c17bb48efa14af792cb83f884167537d
VALENCITOOOO <168395096+VALENCITOOOO@users.noreply.github.com>
2024-05-13 09:57:12 -0600

    Add files via upload

---
+++ contraseña_version2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+//VERSION2 EXAMEN PRACTICO 13-05-24//
+int main() {
+    char nombre[50];
+    char contrasena[50];
+    int intentos = 3;
+   
+
+    do {
+        printf("Introduce tu nombre: ");
+        scanf("%s", nombre);
+
+        printf("Introduce tu contraseña: ");
+        scanf("%s", contrasena);
+
+        if (strcmp(nombre, "root") == 0 && strcmp(contrasena, "2B8") == 0) {
+            printf("Bienvenido al sistema.\n");            
+            break;
+        } else {
+            intentos--;
+            if (intentos > 0) {
+                printf("Nombre de usuario o contraseña incorrectos. Te quedan %d intentos.\n", intentos);
+            } else {
+                printf("Se ha superado el número de intentos permitido.\n");
+                break;
+            }
+    
+    
+       
+        }
+    } while (intentos > 0);
+
+    return 0;
+}
