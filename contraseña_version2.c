26931968b544ac9bb590e8aa197b8ab84bcc9cd1
VALENCITOOOO <168395096+VALENCITOOOO@users.noreply.github.com>
2024-05-13 18:49:06 -0600

    Update contraseña_version2.c

--- contraseña_version2.c
+++ contraseña_version2.c
@@ -14,7 +14,7 @@ int main() {
         printf("Introduce tu contraseña: ");
         scanf("%s", contrasena);
 
-        if (strcmp(nombre, "root") == 0 && strcmp(contrasena, "2B8") == 0) {
+        if (strcmp(nombre, "osvaldo") == 0 && strcmp(contrasena, "2B8") == 0) {
             printf("Bienvenido al sistema.\n");            
             break;
         } else {
