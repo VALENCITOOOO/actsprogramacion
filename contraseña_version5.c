6685122e86e1af6fd79cc2bcc1c5668f9aeb8dea
VALENCITOOOO <168395096+VALENCITOOOO@users.noreply.github.com>
2024-05-13 19:51:14 -0600

    Update contraseña_version5.c

--- contraseña_version5.c
+++ contraseña_version5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-int multiplicacion(articulos, precio){
-    articulos*precio
+int multiplicacion(int articulos, int precio){
+   return articulos*precio;
 }
 //VERSION3 EXAMEN PRACTICO 13-05-24//
 int main() {
@@ -41,9 +41,9 @@ int main() {
                 if (articulos < 0 || precio < 0) {
                     printf("la cantidad y precio deben ser numeros positivos");
                 } else {                    
-                  total= multiplicacion(articulos, precio)
+                
                 }
-                printf("el total  es de : %i ", total );            
+                printf("el total  es de : %i ", multiplicacion(articulos, precio));            
            
      return 0;
 }
